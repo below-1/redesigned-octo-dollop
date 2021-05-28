dbbc4629fafcb745aa02a6b58fc6346c54317a51
jordan meta <jordanmeta@mail.com>
2021-05-28 16:57:11 +0800

    change purge matching rule

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 // only needed if you want to purge
 const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./src/**/*.svelte", "./src/**/*.html"],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: content => [
+    ...(content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []),
+    ...(content.match(/(?<=class:)[^=>\/\s]*/g) || [])
+  ]
 });
 
 module.exports = {
